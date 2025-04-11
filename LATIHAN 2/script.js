281eb1db6855a79974852bb79ed08cbd6597a6f2
ATORII <petermaleke09@gmail.com>
2025-04-12 03:41:29 +0700

    Create script.js

---
+++ LATIHAN 2/script.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question: "Apa ibu kota Indonesia?",
+        options: ["Jakarta", "Surabaya", "Bandung", "Medan"],
+        correct: 0
+    },
+    {
+        question: "Siapa penemu lampu pijar?",
+        options: ["Nikola Tesla", "Thomas Edison", "Albert Einstein", "Alexander Graham Bell"],
+        correct: 1
+    },
+    {
+        question: "Planet terbesar di tata surya adalah?",
+        options: ["Bumi", "Mars", "Jupiter", "Venus"],
+        correct: 2
+    }
+];
